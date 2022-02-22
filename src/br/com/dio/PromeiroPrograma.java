e194d617501b2cb2575035fe2c0e66599c1e4e17
Anselmo Duarte <anselmoduarte6@hotmail.com>
2022-02-22 20:02:24 -0300

    adiconando mais uma linha de sysout.

--- src/br/com/dio/PromeiroPrograma.java
+++ src/br/com/dio/PromeiroPrograma.java
@@ -10,10 +10,10 @@ public class PromeiroPrograma {
 		System.out.println(gato);
 		System.out.println(livros);
 		
-		/*int a = 2;
+		int a = 2;
 		int b = 3;
 		System.out.println("Hello world! " + (a+b));
-		*/
+		
 		//save updates
 	}
 
